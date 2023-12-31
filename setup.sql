94397081df89a8156b14041f5460bc23288643ef
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-31 11:09:03 +0100

    Add ApplicationsAreas table to setup.sql

--- setup.sql
+++ setup.sql
@@ -4,3 +4,8 @@ CREATE TABLE Algorithms (
     Type VARCHAR(255) NOT NULL,
     Complexity VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE ApplicationsAreas (
+    AreaID INT NOT NULL AUTO_INCREMENT,
+    AreaName VARCHAR(255) NOT NULL
+);
