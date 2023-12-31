14cf75a76889595be3caadec11143f68646a9606
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-31 11:10:31 +0100

    Add AlgorithmApplication table to setup.sql

--- setup.sql
+++ setup.sql
@@ -9,3 +9,11 @@ CREATE TABLE ApplicationsAreas (
     AreaID INT NOT NULL AUTO_INCREMENT,
     AreaName VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE AlgorithmApplication (
+    AlgorithmID INT NOT NULL,
+    AreaID INT NOT NULL,
+    PRIMARY KEY (AlgorithmID, AreaID),
+    FOREIGN KEY (AlgorithmID) REFERENCES Algorithms(AlgorithmID),
+    FOREIGN KEY (AreaID) REFERENCES ApplicationsAreas(AreaID)
+);
